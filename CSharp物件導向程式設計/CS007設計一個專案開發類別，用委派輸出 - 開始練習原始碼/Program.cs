b407fde8a773e0fa0c6ce5b4958b2274a4175a28
Vulcan <vulcan.lee@gmail.com>
2021-03-01 10:26:19 +0800

    CS007|設計一個專案開發類別，用委派輸出 - 開始練習原始碼

---
+++ CSharp物件導向程式設計/CS007設計一個專案開發類別，用委派輸出 - 開始練習原始碼/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CS007設計一個專案開發類別_用委派輸出___開始練習原始碼
+{
+    // 宣告一個 Developer 類別
+    class Developer
+    {
+        public string FullName { get; set; }
+        public string Technologies { get; set; }
+    }
+    // 宣告一個 ProjectTeam 類別
+    class ProjectTeam
+    {
+        public List<Developer> Developers { get; set; } = new List<Developer>();
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 建立專案團隊物件與相關開發人員
+            ProjectTeam projectTeam = new ProjectTeam()
+            {
+                Developers = new List<Developer>()
+                {
+                    new Developer(){ FullName = "張小三",  Technologies = "C#, .NET, ASP.NET, SQL Server, WPF, MVVM, MVC, HTML, CSS" },
+                    new Developer(){ FullName = "李小四",  Technologies = "C#, Windows Forms, WPF" },
+                    new Developer(){ FullName = "王小明",  Technologies = "SQL Server" },
+                    new Developer(){ FullName = "店小二",  Technologies = "ASP.NET, HTML, CSS" }
+                }
+            };
+
+            Console.WriteLine("Press any key for continuing...");
+            Console.ReadKey();
+        }
+    }
+}
