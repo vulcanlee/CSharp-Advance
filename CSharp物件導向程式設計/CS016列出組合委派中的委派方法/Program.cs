a60072ef1e8ff5b39fa3bab94c2de7a3aec9c61b
Vulcan <vulcan.lee@gmail.com>
2021-03-01 22:06:45 +0800

    CS016|列出組合委派中的委派方法

---
+++ CSharp物件導向程式設計/CS016列出組合委派中的委派方法/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace CS016列出組合委派中的委派方法
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            Action myHandler = null;
+            myHandler += StaticMethod;
+            myHandler += program.InstanceMethod1;
+            myHandler += program.InstanceMethod2;
+            myHandler += program.InstanceMethod1;
+            foreach (var item in myHandler.GetInvocationList())
+            {
+                string methodType = item.Method.IsStatic == true ? "靜態方法" : "執行個體方法";
+                Console.WriteLine($"{methodType} / {item.Method.Name}" +
+                    $" / {item.Target?.GetType().Name}");
+            }
+
+            Console.WriteLine("Press any key for continuing...");
+            Console.ReadKey();
+        }
+        public static void StaticMethod()
+        {
+        }
+        public void InstanceMethod1()
+        {
+        }
+        public void InstanceMethod2()
+        {
+        }
+    }
+}
