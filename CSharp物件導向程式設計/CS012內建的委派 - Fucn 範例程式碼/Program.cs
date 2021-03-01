67cee63da6891a44775cf8e60786a28d98f4e351
Vulcan <vulcan.lee@gmail.com>
2021-03-01 20:07:09 +0800

    CS012|內建的委派 - Fucn 範例程式碼

---
+++ CSharp物件導向程式設計/CS012內建的委派 - Fucn 範例程式碼/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace CS012內建的委派___Fucn_範例程式碼
+{
+    delegate int MyFuncDelegate(string message);
+    class MyActionClass
+    {
+        // 使用 Func 宣告一個委派變數
+        Func<string, int> MyFuncHandler;
+        // 使用 delegate 自訂型別來宣告一個委派變數
+        MyFuncDelegate MyDelegateHandler;
+        public MyActionClass()
+        {
+            MyFuncHandler = this.MyInstanceMethod;
+            MyFuncHandler = MyActionClass.MyStaticMethod;
+            MyDelegateHandler = this.MyInstanceMethod;
+            MyDelegateHandler = MyActionClass.MyStaticMethod;
+        }
+        // 執行個體方法
+        public int MyInstanceMethod(string message)
+        {
+            return message.Length;
+        }
+        // 靜態方法
+        public static int MyStaticMethod(string message)
+        {
+            return message.Length;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
