26de13776cb9673bd53eb6553e5ca080cd32eab2
Vulcan <vulcan.lee@gmail.com>
2021-03-02 22:37:44 +0800

    CS022|委派變數可以任意設定委派方法、事件卻做不到 - 任意設定委派方法

---
+++ CSharp物件導向程式設計/CS022委派變數可以任意設定委派方法、事件卻做不到 - 任意設定委派方法/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace CS022委派變數可以任意設定委派方法_事件卻做不到___任意設定委派方法
+{
+    class MyDelegateVSEvent
+    {
+        public Action<string> MyHandler;
+        public event Action<string> MyEventHandler;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDelegateVSEvent myDelegateVSEvent = new MyDelegateVSEvent();
+            myDelegateVSEvent.MyHandler += MyMethod;
+            myDelegateVSEvent.MyHandler = null;
+            myDelegateVSEvent.MyEventHandler += MyMethod;
+            // 對於 event 僅能夠透過 += , -= 來加入或移除委派方法
+            //myDelegateVSEvent.MyEventHandler = null;
+        }
+        static void MyMethod(string message)
+        {
+            Console.WriteLine($"{message}");
+        }
+    }
+}
