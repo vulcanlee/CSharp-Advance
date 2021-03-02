68b3d305c98979aa976bf85dedfc0123e2ec0767
Vulcan <vulcan.lee@gmail.com>
2021-03-02 22:43:00 +0800

    CS024|在類別內委派變數可以在外部呼叫，在類別內事件僅能夠在類別內呼叫

---
+++ CSharp物件導向程式設計/CS024在類別內委派變數可以在外部呼叫，在類別內事件僅能夠在類別內呼叫/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace CS024在類別內委派變數可以在外部呼叫_在類別內事件僅能夠在類別內呼叫
+{
+    class MyDelegateVSEvent
+    {
+        public Action<string> MyHandler;
+        public event Action<string> MyEventHandler;
+        public void CallEvent()
+        {
+            MyEventHandler("From internal class");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDelegateVSEvent myDelegateVSEvent = new MyDelegateVSEvent();
+            myDelegateVSEvent.MyHandler += MyMethod;
+            myDelegateVSEvent.MyHandler("From Delegate");
+            myDelegateVSEvent.MyEventHandler += MyMethod;
+            // 對於 event 僅能夠在宣告類別內執行委派方法
+            //myDelegateVSEvent.MyEventHandler("From Delegate");
+        }
+        static void MyMethod(string message)
+        {
+            Console.WriteLine($"{message}");
+        }
+    }
+}
