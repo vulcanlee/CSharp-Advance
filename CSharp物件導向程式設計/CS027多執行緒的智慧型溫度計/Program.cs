68181af94ec21c2714e1c92289736a2c8e70553f
Vulcan <vulcan.lee@gmail.com>
2021-03-02 22:55:42 +0800

    CS027|多執行緒的智慧型溫度計

---
+++ CSharp物件導向程式設計/CS027多執行緒的智慧型溫度計/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading;
+
+namespace CS027多執行緒的智慧型溫度計
+{
+    class SmartThermometer
+    {
+        Random random = new Random();
+        bool RunningStatus = false;
+        int GetCurrentTemperature() => random.Next(15, 50);
+        public void TurnOn()
+        {
+            RunningStatus = true;
+            ThreadPool.QueueUserWorkItem(x =>
+            {
+                while (RunningStatus)
+                {
+                    var currentTemperature = GetCurrentTemperature();
+                    Console.WriteLine($"現在溫度 {currentTemperature} 度");
+
+                    Thread.Sleep(1000);
+                }
+            });
+        }
+        public void TurnOff() => RunningStatus = false;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SmartThermometer smartThermometer = new SmartThermometer();
+            smartThermometer.TurnOn();
+
+            Console.WriteLine("Press any key for continuing...");
+            Console.ReadKey();
+        }
+    }
+}
