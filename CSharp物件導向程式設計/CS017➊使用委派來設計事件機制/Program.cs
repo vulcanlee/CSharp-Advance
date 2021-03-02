8d550d5d22c027b05c42c736008b5934ef2d695b
Vulcan <vulcan.lee@gmail.com>
2021-03-02 21:34:10 +0800

    CS017|➊使用委派來設計事件機制

---
+++ CSharp物件導向程式設計/CS017➊使用委派來設計事件機制/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading;
+
+namespace CS017_使用委派來設計事件機制
+{
+    class Car
+    {
+        int gas = 10;
+        public void Run()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Thread.Sleep(1000);
+                gas--;
+                if (gas == 0)
+                {
+                    Console.WriteLine($"已經沒有汽油了，無法繼續行駛");
+                    return;
+                }
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car();
+            car.Run();
+
+            Console.WriteLine("Press any key for continuing...");
+            Console.ReadKey();
+        }
+    }
+}
